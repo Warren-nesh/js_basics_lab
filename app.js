6fa579c9227dd5217db4d0fc7a1a48e4bcbad68f
@nesh <warrennesh08@gmail.com>
2024-11-13 19:01:56 +0300

    initial commit

---
+++ app.js
@@ -0,0 +1,10 @@
+const b = 1+3
+console.log(b);
+
+let students= [{name:"henry", age:20, class:"form1"},
+               {name:"isaac", age:21, class:"form2"},
+               {name:"Edwin", age:22, class:"form3"},
+               {name:"ian", age:23, class:"form4"},]:
+
+               console.log(students);
+               
